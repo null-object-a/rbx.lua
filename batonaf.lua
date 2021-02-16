5c11653081ceed30fa23264a5f46b09519f80c43
Alfonso <68069359+null-object-a@users.noreply.github.com>
2021-02-16 22:12:28 +0100

    fix more printer grab

--- batonaf.lua
+++ batonaf.lua
@@ -3,8 +3,11 @@ function _load(str)
 end
 _load("https://raw.githubusercontent.com/null-object-a/rbx.lua/main/batonstarter.lua")
 _load("https://raw.githubusercontent.com/Shariiii/Anomic-Stuff/main/Name%20Who.lua")
-_load("https://raw.githubusercontent.com/Shariiii/Anomic-Stuff/main/Baton%20Farm%20Script.lua")
-
+a = 1
+repeat
+    _load("https://raw.githubusercontent.com/Shariiii/Anomic-Stuff/main/Baton%20Farm%20Script.lua")
+  a = a + 1
+until a > 5
 game:GetService("Players").LocalPlayer.OnTeleport:Connect(function(State)
     pcall(function()
         if State == Enum.TeleportState.Started then

0ac5559c193d4191ea93a525c0a13c7df169b884
Alfonso <68069359+null-object-a@users.noreply.github.com>
2021-02-16 20:53:06 +0100

    name hide

--- batonaf.lua
+++ batonaf.lua
@@ -2,7 +2,9 @@ function _load(str)
     loadstring(game:HttpGet(str))()
 end
 _load("https://raw.githubusercontent.com/null-object-a/rbx.lua/main/batonstarter.lua")
+_load("https://raw.githubusercontent.com/Shariiii/Anomic-Stuff/main/Name%20Who.lua")
 _load("https://raw.githubusercontent.com/Shariiii/Anomic-Stuff/main/Baton%20Farm%20Script.lua")
+
 game:GetService("Players").LocalPlayer.OnTeleport:Connect(function(State)
     pcall(function()
         if State == Enum.TeleportState.Started then

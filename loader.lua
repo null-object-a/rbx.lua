cc7ff81280bf0066384cbdcf8b35193146edf3b6
Alfonso <68069359+null-object-a@users.noreply.github.com>
2021-02-18 23:23:07 +0100

    Add files via upload

---
+++ loader.lua
@@ -0,0 +1,11 @@
+function _load(s) 
+    print("Loading: "..s)
+    local data = s
+    loadstring(game:HttpGet(data,true))()
+end
+_load("https://raw.githubusercontent.com/null-object-a/rbx.lua/main/tp.lua")
+_load("https://raw.githubusercontent.com/null-object-a/rbx.lua/main/float.lua")
+_load("https://raw.githubusercontent.com/null-object-a/rbx.lua/main/noclip.lua")
+_load("https://raw.githubusercontent.com/null-object-a/rbx.lua/main/autokick.lua")
+_load("https://raw.githubusercontent.com/null-object-a/rbx.lua/main/anomic%20gui.lua")
+_load("https://raw.githubusercontent.com/null-object-a/rbx.lua/main/chatlogs.lua")

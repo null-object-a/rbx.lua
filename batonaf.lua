1bedd540701aac8d0b04aa39a8e0e199ef6e3a28
Alfonso <68069359+null-object-a@users.noreply.github.com>
2021-02-26 16:18:25 +0100

    Add files via upload

--- batonaf.lua
+++ batonaf.lua
@@ -1,13 +1,12 @@
 function _load(str) 
     loadstring(game:HttpGet(str))()
 end
+wait(10)
 _load("https://raw.githubusercontent.com/null-object-a/rbx.lua/main/batonstarter.lua")
 _load("https://raw.githubusercontent.com/Shariiii/Anomic-Stuff/main/Name%20Who.lua")
-a = 1
-repeat
+for i=1,5 do
     _load("https://raw.githubusercontent.com/Shariiii/Anomic-Stuff/main/Baton%20Farm%20Script.lua")
-  a = a + 1
-until a > 5
+end
 game:GetService("Players").LocalPlayer.OnTeleport:Connect(function(State)
     pcall(function()
         if State == Enum.TeleportState.Started then

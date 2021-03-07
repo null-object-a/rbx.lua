697beab9d9fd700496b91a138ebb7e0e22970802
Alfonso <68069359+null-object-a@users.noreply.github.com>
2021-03-07 14:40:58 +0100

    Update wl.lua

--- wl.lua
+++ wl.lua
@@ -1,1 +1,1 @@
-_G.WL = {"Vxrtan","Wl0n0","XDbruhXDa","santipro3226","omgitdontwork","qtcxpp","ZucaBlyat1","Yourmam144"}
+_G.WL = {"Vxrtan","Wl0n0","XDbruhXDa","santipro3226","omgitdontwork","qtcxpp","ZucaBlyat1","Yourmam144","berrysbot2"}

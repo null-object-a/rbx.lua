4f0cb4f18a240f2edbb3dc85403fa47655b12e1d
Alfonso <68069359+null-object-a@users.noreply.github.com>
2021-03-08 15:04:24 +0100

    Update wl.lua

--- wl.lua
+++ wl.lua
@@ -1,1 +1,1 @@
-_G.WL = {"Vxrtan","Wl0n0","XDbruhXDa","santipro3226","omgitdontwork","qtcxpp","ZucaBlyat1","Yourmam144","berrysbot2","Yu_uix", "epiccoolkid974w"}
+_G.WL = {"Vxrtan","Wl0n0","XDbruhXDa","santipro3226","omgitdontwork","qtcxpp","ZucaBlyat1","Yourmam144","berrysbot2","Yu_uix", "epiccoolkid974w","EpicSynapse_User"}
